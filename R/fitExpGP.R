fd5e4e28f13d79e3a0e13abc09e4a46774a1eec8
Pascal PERNOT <pascal.pernot@u-psud.fr>
2018-10-31 13:46:53 +0100

    Added input params to fitExpGP

--- R/fitExpGP.R
+++ R/fitExpGP.R
@@ -9,6 +9,9 @@
 #' @param cor_theta theta prior correlation matrix
 #' @param ru_theta  theta prior uncertainty scale
 #' @param lambda_rate scale of ctrl points prior
+#' @param alpha_scale SD scale of GP
+#' @param rho_scale relative correlation length of GP
+#' @param gridType type of controle points grid (`internal` does not contain boundaries)
 #' @param lasso flag to use lasso prior
 #' @param iter max. number of iterations for 'optim'
 #' @param prior_PD  flag to sample from prior pdf only
@@ -27,8 +30,8 @@
 #' @details Bayesian inference of the parameters of an exponential
 #' model with modulation assuming an uncorrelated normal noise
 #' \code{y(x) ~ normal(m(x),uy(x));
-#' m(x) = theta[1] + theta[2]*exp(-x/theta[3]*(1+l(x)));}. 
-#' \code{l(x)} is defined by a GP with fixed positions 'xGP', 
+#' m(x) = theta[1] + theta[2]*exp(-x/theta[3]*(1+l(x)));}.
+#' \code{l(x)} is defined by a GP with fixed positions 'xGP',
 #' variance and correlation length.
 #' @export
 
@@ -42,13 +45,22 @@ fitExpGP <- function(x, y, uy,
                      method    = 'sample', # One of 'sample','optim'
                      iter      = 50000,    # Max. iterations of optimizer
                      prior_PD  = 0,        # Flag to sample from prior only
+                     alpha_scale = 0.1,    # SD scale of GP
+                     rho_scale = 1/Nn,     # Relative correlation length of GP
+                     gridType  = 'internal',
                      nb_chains = 4,
                      nb_warmup = 500,
-                     nb_iter   = 1000) {
+                     nb_iter   = 1000,
+                     verbose   = FALSE,
+                     open_progress = TRUE) {
 
   # Grid of GP control points
-  dx  = diff(range(x))/(Nn+1)
-  xGP = seq(min(x)+dx/2,max(x)-dx/2,length.out = Nn)
+  if(gridType == 'internal') {
+    dx  = diff(range(x))/(Nn+1)
+    xGP = seq(min(x)+dx/2,max(x)-dx/2,length.out = Nn)
+  } else {
+    xGP = seq(min(x),max(x),length.out = Nn)
+  }
 
   # Initial monoexp params
   if(is.null(theta0))
@@ -63,8 +75,8 @@ fitExpGP <- function(x, y, uy,
     Np = 3,
     Nn = Nn,
     xGP = xGP,
-    alpha_scale = 0.1,
-    rho_scale   = 0.1,
+    alpha_scale = alpha_scale,
+    rho_scale   = rho_scale,
     theta0      = theta0,
     cor_theta   = cor_theta,
     ru_theta    = ru_theta,
@@ -90,9 +102,6 @@ fitExpGP <- function(x, y, uy,
   if(prior_PD == 0)
     pars   = c(pars,'resid','br','m','dL')
 
-  # model = stanmodels$fitExpGP
-  # if(lasso) model = stanmodels$fitExpGPLasso
-
   # Run Stan
   if(method == 'sample') {
 
@@ -101,8 +110,11 @@ fitExpGP <- function(x, y, uy,
                    pars = pars,
                    init = function() {init},
                    control = list(adapt_delta=0.99,max_treedepth=12),
-                   iter = nb_iter, chains = nb_chains,
-                   warmup = nb_warmup, verbose=FALSE)
+                   iter = nb_iter,
+                   chains = nb_chains,
+                   warmup = nb_warmup,
+                   verbose = verbose,
+                   open_progress = open_progress)
 
   } else {
 
@@ -110,24 +122,25 @@ fitExpGP <- function(x, y, uy,
                      data = stanData,
                      init = init,
                      as_vector = FALSE,
-                     verbose   = FALSE,
                      algorithm = 'LBFGS',
                      hessian = TRUE,
                      draws = 500,
                      iter = iter,
-                     refresh = 500)
+                     refresh = 500,
+                     verbose = verbose)
+
     if(is.null(fit$theta_tilde))
-      # No Hessian-based sampling done => try to refine solution
+      # No Hessian-based sampling done => try once to refine solution
       fit = optimizing(stanmodels$modFitExpGP,
                        data = stanData,
                        init = fit$par, # Restart
                        as_vector = FALSE,
-                       verbose   = FALSE,
                        algorithm = 'LBFGS',
                        hessian = TRUE,
                        draws = 500,
                        iter = iter,
-                       refresh = 500)
+                       refresh = 500,
+                       verbose = verbose)
   }
 
   return(list(fit = fit, method = method, xGP = xGP,
